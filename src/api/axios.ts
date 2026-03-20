ceda5bc527327e5f7cc59f032ea62ade4511fb67
delson-uniqnex360 <delson@uniqnex@gmail.com>
2026-03-20 20:05:28 +0530

    added base url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,6 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000", // 🔥 your FastAPI URL
+  // baseURL: "http://localhost:8000", // 🔥 your FastAPI URL
+  baseURL: "https://searchsphere-6fqs.onrender.com",
   timeout: 10000,
 });
