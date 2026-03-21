a0be5b92eb1922b847aa6429ee9eed32b44c7a16
delson-uniqnex360 <delson@uniqnex@gmail.com>
2026-03-21 14:57:26 +0530

    change base url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://localhost:8000", // 🔥 your FastAPI URL
-  baseURL: "https://35.175.188.201.nip.io",
+  baseURL: "https://18.234.66.152.nip.io",
   timeout: 10000,
 });
