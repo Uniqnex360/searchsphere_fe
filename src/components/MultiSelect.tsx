83a8d41479f5e87567a5c9460b0761aee17a9cd0
delson-uniqnex360 <delson@uniqnex@gmail.com>
2026-04-09 11:32:41 +0530

    added icon for sort and price filer

--- src/components/MultiSelect.tsx
+++ src/components/MultiSelect.tsx
@@ -1,5 +1,6 @@
 import { ChevronDown, X } from "lucide-react";
-import { useEffect, useRef, useState, ReactNode } from "react";
+import { useEffect, useRef, useState } from "react";
+import type { ReactNode } from "react";
 
 interface Props {
   options: string[];
