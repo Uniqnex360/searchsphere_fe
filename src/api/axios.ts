9c540dcdb993979e93be682cc365561805aefc8e
delson-uniqnex360 <delson@uniqnex@gmail.com>
2026-06-04 13:19:06 +0530

    added new base url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://localhost:8000",
-  baseURL: "https://54.91.144.49.nip.io/api-f1",
+  baseURL: "https://54.196.84.222.nip.io/api-f1",
   timeout: 10000,
 });
